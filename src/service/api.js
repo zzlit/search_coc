5146612ab6b50abf91a26fdba7b1f37ac1407d12
zxy <874331618@qq.com>
2019-01-31 17:43:09 +0800

    增加token

--- src/service/api.js
+++ src/service/api.js
@@ -14,7 +14,7 @@ export default {
       url: url.indexOf('http://') === -1 && url.indexOf('https://') === -1 ? request_url + url : url,
       data: data,
       method: method,
-      header: { 'content-type': contentType }
+      header: { 'content-type': contentType, 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZjNTBhZDUwLWU5ZWYtNDBlYi1hYzYyLWEzYWQ2OTQ2ZmJiNyIsImlhdCI6MTU0ODkyNzAzOCwic3ViIjoiZGV2ZWxvcGVyL2M0NGUzYTBiLTIzNGItZjI3OS1jYTAzLTA2MjhmYzc5MTA5OCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjAuMC4wLjAiXSwidHlwZSI6ImNsaWVudCJ9XX0.gakPQu8g_Xvvg94L3Qv3QuPzjXtWw2D9bH2o6cn1ZUl2mS0qg1KF4tA4laQj0wyLox4a2TDKw_b41JZ9CfHYog'}
     }
     return Taro.request(option)
   },
